79cdef8c8bb7776593bfd49250d5e4a3eb6487c5
Wagner Alves <guinn83@hotmail.com>
2022-08-13 21:02:49 -0300

    Project created

---
+++ src/main/java/com/springcourse/course/UserResource.java
@@ -0,0 +1,20 @@
+package com.springcourse.course.resources;
+
+import com.springcourse.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Wagner", "wagner@gmail.com", "9999999", "12345");
+        System.out.println(u);
+        return ResponseEntity.ok().body(u);
+    }
+
+}
---
+++ src/test/java/com/springcourse/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcourse.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
